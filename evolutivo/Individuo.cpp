af4d4b3b352328cb04f40cdf1b35704b1df43889
hcarey <hcarey@hcarey-HP-Mini-110-3700>
2015-10-13 13:20:25 -0500

    "hola"

---
+++ evolutivo/Individuo.cpp
@@ -0,0 +1,18 @@
+/*
+ * Individuo.cpp
+ *
+ *  Created on: 13/10/2015
+ *      Author: hcarey
+ */
+
+#include "Individuo.h"
+
+Individuo::Individuo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Individuo::~Individuo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ evolutivo/Individuo.h
@@ -0,0 +1,17 @@
+/*
+ * Individuo.h
+ *
+ *  Created on: 13/10/2015
+ *      Author: hcarey
+ */
+
+#ifndef INDIVIDUO_H_
+#define INDIVIDUO_H_
+
+class Individuo {
+public:
+	Individuo();
+	virtual ~Individuo();
+};
+
+#endif /* INDIVIDUO_H_ */
