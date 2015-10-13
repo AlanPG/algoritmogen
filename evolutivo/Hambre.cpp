96a880751ec8b837290f88a4ea0b3090e81c3a43
hcarey <hcarey@hcarey-HP-Mini-110-3700>
2015-10-13 14:52:45 -0500

    "ugly"

---
+++ evolutivo/Hambre.cpp
@@ -0,0 +1,18 @@
+/*
+ * Hambre.cpp
+ *
+ *  Created on: 13/10/2015
+ *      Author: hcarey
+ */
+
+#include "Hambre.h"
+
+Hambre::Hambre() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Hambre::~Hambre() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ evolutivo/Hambre.h
@@ -0,0 +1,17 @@
+/*
+ * Hambre.h
+ *
+ *  Created on: 13/10/2015
+ *      Author: hcarey
+ */
+
+#ifndef HAMBRE_H_
+#define HAMBRE_H_
+
+class Hambre {
+public:
+	Hambre();
+	virtual ~Hambre();
+};
+
+#endif /* HAMBRE_H_ */
