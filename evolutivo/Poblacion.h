2cf1832ae940fac6719ffb88e62ec003e9e5e136
hectorCarey <talkingmichael93@gmail.com>
2015-10-15 14:00:44 -0500

    Create Poblacion.h

---
+++ evolutivo/Poblacion.h
@@ -0,0 +1,41 @@
+//poblacion.h
+//Algoritmo Genetico
+//Author: Héctor Rodríguez González
+
+#ifndef _Algoritmo_Genetico_Poblacion
+#define _Algoritmo_Genetico_Poblacion
+
+#include <stdio.h>
+
+
+class Poblacion{
+private:
+    int numIndividuos;
+    float numGeneraciones;
+    Individuo[]poblacion;
+
+
+public:
+    Poblacion(){
+
+    }
+
+    Poblacion(int, float,Individuo[]){
+        int getnumIndividuos();
+        float getNumGeneraciones();
+        Individuo[] getpoblacion();
+
+        void setnumIndividuos(int);
+        void setnumGeneraciones(float);
+        void setpoblacion(Individuo[]);
+
+    }
+
+    
+
+//    void creaPobInicial();
+
+
+};
+
+#endif 
