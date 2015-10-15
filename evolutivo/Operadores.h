ad18dd5d523dd96877ff2ac1159bc3e1129b41c5
hectorCarey <talkingmichael93@gmail.com>
2015-10-15 14:01:36 -0500

    Create Operadores.h

---
+++ evolutivo/Operadores.h
@@ -0,0 +1,24 @@
+//Operadores.h
+//Algoritmo Genetico
+//Author: Héctor Rodríguez González Héctor
+
+#ifndef _Algoritmo_Genetico_Operadores
+#define _Algoritmo_Genetico_Operadores
+
+#include <stdio.h>
+
+class Operadores{
+public:
+	Operadores(){
+
+	}
+
+	void seleccion(){}
+
+	void mutacion(){}
+
+	void cruza(){}
+
+};
+
+#endif 
